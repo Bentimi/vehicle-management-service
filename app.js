31606c02a77a5d6ea796d66c82eb608738796338
Ogunkunle Benjamin Timileyin <bentimi02@gmail.com>
2026-03-29 21:47:41 +0100

    frontend url

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cors({
         'http://localhost:5173', 
         'http://localhost:5174', 
         'http://localhost:5175', 
-        process.env.FRONTEND
+        "https://intelligent-vehicle-management-syst.vercel.app"
     ], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
