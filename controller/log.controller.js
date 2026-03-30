841912a57ff3b55f55d70dfbf53f6746e1124b81
Ogunkunle Benjamin Timileyin <bentimi02@gmail.com>
2026-03-30 11:06:26 +0100

    logs search

--- controller/log.controller.js
+++ controller/log.controller.js
@@ -33,8 +33,9 @@ const get_logs = async (req, res, next) => {
         const userId = req.user.id;
         const page = req.query.page || 1;
         const pageSize = req.query.pageSize || 10;
+        const search = req.query.search || "";
         
-        const result = await logService.getAllLogs(page, pageSize, userId);
+        const result = await logService.getAllLogs(page, pageSize, userId, search);
         res.success(result, "Logs successfully retrieved");
     } catch (e) {
         next(e);
