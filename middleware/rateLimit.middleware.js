06b846a389b490295704c2ba44396b41ee0d9e92
Ogunkunle Benjamin Timileyin <bentimi02@gmail.com>
2026-04-08 19:56:50 +0100

    rate limit

--- middleware/rateLimit.middleware.js
+++ middleware/rateLimit.middleware.js
@@ -35,19 +35,29 @@ const createRateLimiterMiddleware = (name, points, duration) => {
                 return next();
             }
 
-            res.set({
-                "X-RateLimit-Limit": points,
-                "X-RateLimit-Remaining": 0,
-                "X-RateLimit-Reset": new Date(Date.now() + rejRes.msBeforeNext).getTime() / 1000,
-                "Retry-After": Math.round(rejRes.msBeforeNext / 1000) || 1
-            });
-            
-            res.status(429).json({
-                success: false,
-                message: "Too many requests, please try again later.",
-                retry_after_seconds: Math.round(rejRes.msBeforeNext / 1000) || 1
-            });
-        }
+            const totalSeconds = Math.round(rejRes.msBeforeNext / 1000) || 1;
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds % 60;
+
+        const formattedTime =
+            minutes > 0
+                ? `${minutes}m:${String(seconds).padStart(2, '0')}s`
+                : `${seconds}s`;
+
+        res.set({
+            "X-RateLimit-Limit": points,
+            "X-RateLimit-Remaining": 0,
+            "X-RateLimit-Reset": new Date(Date.now() + rejRes.msBeforeNext).getTime() / 1000,
+            "Retry-After": totalSeconds
+        });
+
+        res.status(429).json({
+            success: false,
+            message: `You're sending requests too fast. Try again in ${formattedTime}.`,
+            retry_after_seconds: totalSeconds,
+            retry_after_formatted: formattedTime
+        });
+        };
     };
 };
 
@@ -58,7 +68,7 @@ const anonymousLimiter = createRateLimiterMiddleware("anonymous_general", 30, 15
 
 const signUpLimiter = createRateLimiterMiddleware("signup", 5, 15 * 60);
 
-const signInLimiter = createRateLimiterMiddleware("signin", 10, 15 * 60);
+const signInLimiter = createRateLimiterMiddleware("signin", 7, 10 * 60);
 
 const authenticatedLimiter = createRateLimiterMiddleware("authenticated_general", 1000, 15 * 60);
 
